58631f6e5ca8ad08bd80388760f9ee29c0cdde66
Marc Jimenez <marc.jimenez@perficient.com>
2022-01-28 16:13:59 -0600

    Seperated the xbox test cases adding correct Test ending and updated pom

--- src/test/java/xbox/testcases/XboxVerifyGamerTest.java
+++ src/test/java/xbox/testcases/XboxVerifyGamerTest.java
@@ -14,46 +14,8 @@ import java.util.HashMap;
 import xbox.basepages.XboxGamertagPage;
 import xbox.basepages.XboxHomePage;
 
-public class XboxVerify extends TestCaseBase {
-
-	@Test(groups = {"ChromeWin32", "IEWin32"})
-	public void open() throws InterruptedException {
-		
-		Properties PROPERTIES_RESOURCES = SystemUtil.loadPropertiesResources("/testdata_xbox.properties");
-		String firstPlaceGT = PROPERTIES_RESOURCES.getProperty("firstPlaceGT");
-		String secondPlaceGT = PROPERTIES_RESOURCES.getProperty("secondPlaceGT");
-		String thirdPlaceGT = PROPERTIES_RESOURCES.getProperty("thirdPlaceGT");
-		String fourthPlaceGT = PROPERTIES_RESOURCES.getProperty("fourthPlaceGT");
-		String fifthPlaceGT = PROPERTIES_RESOURCES.getProperty("fifthPlaceGT");
-		
-		HashMap<String, String> Gamertags = new HashMap<String, String>();
-		Gamertags.put("firstPlaceGT", firstPlaceGT);
-		Gamertags.put("secondPlaceGT", secondPlaceGT);
-		Gamertags.put("thirdPlaceGT", thirdPlaceGT);
-		Gamertags.put("fourthPlaceGT", fourthPlaceGT);
-		Gamertags.put("fifthPlaceGT", fifthPlaceGT);
-		
-		XboxHomePage home = new XboxHomePage();
-		CustomAssertion assertion = new CustomAssertion();
-		home.open();
-		home.waitPage();
-		Assert.assertTrue(home.titleIs(home.TITLE));
-		
-		String firstPlaceTag = home.getFirstPlace();
-		String secondPlaceTag = home.getSecondPlace();
-		String thirdPlaceTag = home.getThirdPlace();
-		String fourthPlaceTag = home.getFourthPlace();
-		String fifthPlaceTag = home.getFifthPlace();
-		
-		assertion.equals(firstPlaceTag, Gamertags, "firstPlaceGT");
-		assertion.equals(secondPlaceTag, Gamertags, "secondPlaceGT");		
-		assertion.equals(thirdPlaceTag, Gamertags, "thirdPlaceGT");		
-		assertion.equals(fourthPlaceTag, Gamertags, "fourthPlaceGT");		
-		assertion.equals(fifthPlaceTag, Gamertags, "fifthPlaceGT");		
-		
-	}
+public class XboxVerifyGamerTest extends TestCaseBase {
 	
-
 	@Test(groups = {"ChromeWin32", "IEWin32"})
 	public void openNewGamer() throws InterruptedException {
 		XboxHomePage home = new XboxHomePage();
@@ -97,4 +59,3 @@ public class XboxVerify extends TestCaseBase {
 
 	}
 }
-
